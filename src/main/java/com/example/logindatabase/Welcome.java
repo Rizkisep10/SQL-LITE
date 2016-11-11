c2671da33f526fe1373a3da61e883a55459fe72c
rizkisep10 <rizkiseptian@student.uinsgd.ac.id>
2016-11-11 11:10:07 +0700

    versi 1.0.0

---
+++ src/main/java/com/example/logindatabase/Welcome.java
@@ -0,0 +1,18 @@
+package com.example.logindatabase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by septian on 11/11/2016.
+ */
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
+
